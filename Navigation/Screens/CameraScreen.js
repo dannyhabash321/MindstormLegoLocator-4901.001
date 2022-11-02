b5fbdc221c44fdc5e93f4f89e324a4bcca7f94f4
David Chacon <davidsilverxiii@gmail.com>
2022-11-01 19:46:58 -0500

    basic camera setup

--- Navigation/Screens/CameraScreen.js
+++ Navigation/Screens/CameraScreen.js
@@ -1,57 +1,62 @@
-import { color } from '@rneui/base';
-import * as React from 'react';
-import { StyleSheet, Text, View, ScrollView, TouchableWithoutFeedback, TouchableOpacity, Image } from 'react-native';
-import { Surface } from 'react-native-paper';
-import { Stack, Button,FAB } from "@react-native-material/core";
-import Icon from 'react-native-vector-icons/AntDesign';
-function CameraScreen({navigation}){
-    return(
-        <View style={styles.container}>
-         <Icon.Button name="left" style={styles.button} color="#ff0000" backgroundColor="#ffffff" size="30"> 
-         <Text style={{fontSize: 25, color:"#ff0000" }}>Home</Text>
-        </Icon.Button>
-        <View style={styles.main}>
+import React, { useState, useEffect } from 'react';
+import { StyleSheet ,Text, View, Button, Image} from 'react-native';
+import { Camera } from 'expo-camera';
+
+export default function CameraScreen({navigation}) {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+ 
+ 
+useEffect(() => {
+    (async () => {
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted');
+})();
+  }, []);
+const takePicture = async () => {
+    if(camera){
+        const data = await camera.takePictureAsync(null)
+        setImage(data.uri);
+    }
+  }
+  // const removePicture = async () => {
+  
+
+  if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+   <View style={{ flex: 1}}>
+      <View style={styles.cameraContainer}>
+            <Camera 
+            ref={ref => setCamera(ref)}
+            style={styles.cameraContainer} 
+            type={type}
+            />
+      </View>
+      <Button
+            title="Flip Camera"
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+        </Button>
+       <Button title="Take Picture" onPress={() => takePicture()} />
+        {image && <Image source={{uri: image}} style={{flex:1}}/>}
+        {/* <Button title="x" onPress={() => removePicture()} /> */}
         
-        <Stack fill center spacing={10}>
-        <Surface
-        elevation={4}
-        style={{
-        justifyContent: "center",
-        alignItems: "center",
-        width: 300,
-        height: 300,
-        borderRadius:20,
-        marginTop:50
-      }}
-    >
-      <Text style={{  fontSize: 32, textAlign: "center",fontWeight:"bold", margin:10}}>Would you like to use Picture Mode or Live Feed Mode?</Text>
-        </Surface>
-        <Stack fill center spacing={50}>
-    <FAB variant='extended' color="#ff0000" label="Picture Mode" 
-    labelStyle={{fontSize: 20, textAlign: "center",fontWeight:"bold",color:"#ffffff"}}
-    contentContainerStyle={{width:300, alignItems:"center",justifyContent:"center"}} />
-    <FAB variant='extended' color="#ff0000" label="Live Feed Mode" 
-    labelStyle={{fontSize: 20, textAlign: "center",fontWeight:"bold",color:"#ffffff"}}
-    contentContainerStyle={{width:300, alignItems:"center",justifyContent:"center"}} />
-    </Stack>
-  </Stack>
-    </View>
-    </View>
-    )
+   </View>
+  );
 }
 const styles = StyleSheet.create({
-    container: {
+  cameraContainer: {
       flex: 1,
-      backgroundColor: '#fff',
-      
-    },
-    button: {
-        marginTop:50  
-    },
-    main:{
-      flex:2,
-      justifyContent: "center",
-      alignItems: "center",
-    }
-  });
-export default CameraScreen
+      flexDirection: 'row'
+  },
+})
